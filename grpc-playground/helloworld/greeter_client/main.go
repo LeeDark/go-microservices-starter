ef8998a7b9fc4c3af6df067c45e86715b5b91893
Sergey Povarnin <sergey.povarnin@gmail.com>
2026-06-17 13:37:34 +0300

    grpc-playground: Phase 2. gRPC in Go - Quick start. Update the gRPC service.

--- grpc-playground/helloworld/greeter_client/main.go
+++ grpc-playground/helloworld/greeter_client/main.go
@@ -52,7 +52,14 @@ func main() {
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+
+	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
